6542ad260bccb4987fef93d488903cfd73b942ec
Basil Haidi <basilhaidi65@gmail.com>
2022-07-09 20:05:26 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 const App = () => {
+  // ######### CARA 1: MAPPING DI LUAR RETURN COMPONEN #######
+  // const dataContacts = contacts.map((data) => {
+  //   return (
+  //     <Contact
+  //       key={data.key}
+  //       name={data.name}
+  //       phone={data.phone}
+  //       email={data.email}
+  //       photo={data.photo}
+  //     />
+  //   );
+  // });
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {/* ######### CARA 2: MAPPING DI DALAM RETURN COMPONEN #######
+      {contacts.map((data) => {
+        return (
+          <Contact
+            key={data.key}
+            name={data.name}
+            phone={data.phone}
+            email={data.email}
+            photo={data.photo}
+          />
+        );
+      })} */}
+      <div className="master-card">
+        <Contact data={contacts[0]} />
+        <Contact data={contacts[1]} />
+        <Contact data={contacts[2]} />
+        <Contact data={contacts[3]} />
+        <Contact data={contacts[4]} />
+        <Contact data={contacts[5]} />;
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  const { name, phone, email, photo } = data;
+  return (
+    <div className="card">
+      <div>
+        <img src={photo} alt="foto_kucing" width="200" height="200"></img>
+      </div>
+      <div>
+        <b>
+          <p>{name}</p>
+        </b>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
